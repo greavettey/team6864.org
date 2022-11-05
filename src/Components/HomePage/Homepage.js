0e846cec83b2a8c955cb2da2278f977491393d43
Trent Marriott <trent.marriott@outlook.com>
2022-11-04 20:52:11 -0400

    redid trhe landing page still default assets some styling needs to be imporved in the future

--- src/Components/HomePage/Homepage.js
+++ src/Components/HomePage/Homepage.js
@@ -1,29 +1,32 @@
 import React from "react";
 import "../../Styles/HomePage/Homepage.css";
-import { LoremIpsum } from "react-lorem-ipsum";
 
 const Homepage = () => {
   return (
-    <section>
-      <div className="header-container">
-        <img
-          src={require("../../Images/Test-image.jpg")}
-          alt="could not load image"
-        />
-        <div className="text-container">
-          <h1>Team 6864 Gryphtech Robotics</h1>
-        </div>
+    <section className="home-page-container">
+      <img
+        src={require("../../Images/Test-bg-image.jfif")}
+        alt=""
+        className="bg-image"
+      />
+      <section className="header-container">
+        <p>about</p>
+        <p>about</p>
+        <p>about</p>
+        <p>about</p>
+      </section>
+      <img
+        src={require("../../Images/Gryphtech logo.png")}
+        alt=""
+        className="logo"
+      />
+      <div className="team-name-container">
+        <h2>Team 6864</h2>
+        <h2>We Are Gryphtech Robotics</h2>
       </div>
-      <body className="home-container">
-        <div className="left">
-          <h3>about us</h3>
-          <p> {<LoremIpsum />}</p>
-        </div>
-        <div className="right">
-          <h3>Our Robot</h3>
-          <p>{<LoremIpsum />}</p>
-        </div>
-      </body>
+      <div className="arrow-container">
+        <h3>&#709;</h3>
+      </div>
     </section>
   );
 };
