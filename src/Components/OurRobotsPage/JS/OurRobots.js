d248c23a6f6e61f3f95ea013511bbe1bdba3a26d
WavyBTW11 <97416733+WavyBTW11@users.noreply.github.com>
2022-11-29 14:33:37 -0500

    added the our robots page with blank assets

--- src/Components/OurRobotsPage/JS/OurRobots.js
+++ src/Components/OurRobotsPage/JS/OurRobots.js
@@ -1,7 +1,35 @@
 import React from "react";
+import LoremIpsum from "react-lorem-ipsum";
+import "../Styles/OurRobots.css";
 
 const OurRobots = () => {
-  return <div>OurRobots</div>;
+  return (
+    <section className="our-robot-container">
+      <h1>Our Robots</h1>
+
+      <p className="discription">{<LoremIpsum />}</p>
+
+      <div className="card-container">
+        <div className="card">
+          <img src={require("../../../Images/Unknown.jpeg")} alt="" />
+
+          <div className="txt-container">
+            <h2>Robot 1</h2>
+            <p>{<LoremIpsum length={1} />}</p>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src={require("../../../Images/Unknown.jpeg")} alt="" />
+
+          <div className="txt-container">
+            <h2>Robot 1</h2>
+            <p>{<LoremIpsum length={1} />}</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default OurRobots;
