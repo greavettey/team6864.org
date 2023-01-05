a6a54788ef2513b363b4f77d439326542afcf661
Axel <axelgreavette@gmail.com>
2023-01-05 22:34:14 +0000

    Changes - thundercat

--- src/Components/AboutPage/JS/About.js
+++ src/Components/AboutPage/JS/About.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "../Styles/About.css";
-import LoremIpsum from "react-lorem-ipsum";
 
 const About = () => {
   return (
@@ -11,13 +10,9 @@ const About = () => {
         <div className="text-box">
           <h2>Our Team</h2>
           <p>
-            Gryphtech Robotics is a robotics team based out of a local high
-            school in Gravenhurst. We compete in competitions held by FIRST
-            Robotics every year. Our group of around 30 students create a new
-            robot every year to compete in these competitions. We have a build
-            team, programming team, fundraising/media team and a CAD team. Even
-            though we have a multitude of different teams, anyone can do
-            anything and we all work together to reach our end goals.
+            Gryphtech Robotics is a robotics team based out of Gravenhurst High School in beautiful Muskoka.
+            We've competed in FIRST Robotics compitition across Ontario in the past 5 years. Our group of around 30 students create a new
+            robot every year to compete in these competitions. Comprised of 30+ members, students are invited to take part in all aspects of FIRST, from media and PR, to building and programming the robot, and together we form a close team.
           </p>
         </div>
 
@@ -31,22 +26,10 @@ const About = () => {
             use. We had no access to 3D printers or CNC machines and had to cut
             the pieces for our robot by hand. Over the years we got more money,
             more tools and more support. Now we have our own classroom with all
-            sorts of power tools, 3D printers and even our one CNC machine.
+            sorts of power tools, 3D printers and even our own CNC machine.
           </p>
         </div>
       </section>
-
-      <div className="text-container">
-        <div className="text-box">
-          <h2>...</h2>
-          <p>{<LoremIpsum />}</p>
-        </div>
-
-        <div className="text-box">
-          <h2>...</h2>
-          <p>{<LoremIpsum />}</p>
-        </div>
-      </div>
     </section>
   );
 };
